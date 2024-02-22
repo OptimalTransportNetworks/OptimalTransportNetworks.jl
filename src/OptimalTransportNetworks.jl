4478599c6a4791dde86b618863c3468517748a3e
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-02-22 13:21:23 -0800

    Fix bulk loading files.

--- src/OptimalTransportNetworks.jl
+++ src/OptimalTransportNetworks.jl
@@ -6,22 +6,34 @@ using LinearAlgebra, Statistics, Ipopt, JuMP
 
 # Function to include all .jl files in a directory
 function include_directory(directory)
-    for file in readdir(directory)
-        if endswith(file, ".jl")
-            include(joinpath(directory, file))
+    # Use the path of the current file to construct the directory path
+    dir_path = joinpath(dirname(@__FILE__), directory)
+    # Check if directory exists
+    if isdir(dir_path)
+        for file in readdir(dir_path)
+            if endswith(file, ".jl")
+                # Include the file using its full path
+                include(joinpath(dir_path, file))
+            end
         end
+    else
+        error("Directory does not exist: $dir_path")
     end
 end
 
+
 # Include all .jl files in a specific directory
-include_directory("src/objective")
-include_directory("src/constraints")
-include_directory("src/solve_allocation")
-include_directory("src/main")
+include_directory("objective")
+include_directory("constraints")
+include_directory("solve_allocation")
+include_directory("main")
 
 # Defining exports
-export init_parameters, create_graph, plot_graph, create_auxdata, 
+export init_parameters, create_graph, plot_graph, create_auxdata
 export optimal_network, annealing, solve_allocation, call_adigator
 export add_node, find_node, remove_node, apply_geography
 
 end
+
+# Load using: 
+#  using .OptimalTransportNetworks
