e6996e45508de8b941bed469d9114c646cd7e15f
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-02-26 23:30:27 -0800

    Adding kappa_extract here and describing A.

--- src/main/create_auxdata.jl
+++ src/main/create_auxdata.jl
@@ -23,6 +23,9 @@ function create_auxdata(param, graph, I)
     kappa_ex = kappa_extract(graph, kappa)  # extract the ndeg free values of matrix kappa (due to symmetry)
 
     # Create matrix A
+    # Note: matrix A is of dimension J*ndeg and takes value 1 when node J is
+    # the starting point of edge ndeg, and -1 when it is the ending point. It 
+    # is used to make hessian and jacobian calculations using matrix algebra
     A = zeros(graph.J, graph.ndeg)
     id = 1
     for j in 1:graph.J
@@ -53,3 +56,22 @@ end
 
 
 # Please note that the translation assumes that `param`, `graph`, and `I` are dictionaries or custom types with the appropriate fields. The `graph.nodes` field is assumed to be an array of custom types or dictionaries with a `neighbors` field. The `!` operator in Julia is equivalent to the `~` operator in Matlab, and the `.` operator is used for element-wise operations. The `max` function is used with the `.` operator for element-wise comparison. The `+=` operator is used for incrementing `id`. The `Dict` function is used to create a dictionary for `auxdata`.
+
+"""
+    kappa_extract(graph, kappa)
+
+Description: auxiliary function that converts kappa_jk into kappa_i
+"""
+function kappa_extract(graph, kappa)
+    kappa_ex = zeros(graph.ndeg)
+    id = 1
+    for i in 1:graph.J
+        for j in graph.nodes[i]
+            if j > i
+                kappa_ex[id] = kappa[i, j]
+                id += 1
+            end
+        end
+    end
+    return kappa_ex
+end
