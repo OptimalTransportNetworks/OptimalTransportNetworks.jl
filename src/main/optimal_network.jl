56d629231e1bce6b1a2801a190b6982c68590bc8
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-02-26 23:29:11 -0800

    Misc implementing the JuMP solution.

--- src/main/optimal_network.jl
+++ src/main/optimal_network.jl
@@ -35,7 +35,6 @@ This code is distributed under BSD-3 License. See LICENSE.txt for more informati
 function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbose=false, x0=nothing)
 
     J = graph.J
-    save_before_it_crashes = false
     TOL_I_BOUNDS = 1e-7
     error_status = false
 
@@ -43,7 +42,7 @@ function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbo
     # CHECK PARAMETERS FOR ERRORS OR MISSING VALUES
 
     if param[:a] > 1
-        error("Model with increasing returns to scale not convex.")
+        error("Model with increasing returns to scale are not convex.")
     end
 
     if param[:nu] < 1 && param[:cong]
@@ -71,60 +70,61 @@ function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbo
     # --------------
     # INITIALIZATION
 
-    # CUSTOMIZATION 1: provide here the initial point of optimization for custom case
+    optimizer = get(param, :optimizer, Ipopt.Optimizer)
+    auxdata = create_auxdata(param, graph, I0)
+
+    # if param[:adigator] && param[:mobility] != 0.5
     if param[:custom]
-        x0 = [1e-6 * ones(Int64, graph.J * param[:N]); zeros(Int64, graph.ndeg * param[:N]); sum(param[:Lj]) / (graph.J * param[:N]) * ones(Int64, graph.J * param[:N])]
-        # Based on the primal case with immobile and no cross-good congestion, to
-        # be customized
+        model = param[:custom_model](optimizer, auxdata)
+    elseif param[:mobility] == 1 && param[:cong]
+        model = model_mobility_cgc(optimizer, auxdata)
+    elseif param[:mobility] == 0 && param[:cong]
+        model = model_fixed_cgc(optimizer, auxdata)
+    elseif param[:mobility] == 1 && !param[:cong]
+        model = model_mobility(optimizer, auxdata)
+        recover_allocation = recover_allocation_mobility
+    elseif (param[:mobility] == 0 && !param[:cong]) && (param[:beta] <= 1 && param[:a] < 1 && param[:duality])
+        # TODO: Dual solution !!
+        solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_by_duality_ADiGator(x0, auxdata, funcs, verbose)
+    else 
+        model = model_fixed(optimizer, auxdata)
     end
-    # END OF CUSTOMIZATION 1
-
-    # -------------
-    # Call ADiGator
-
-    funcs = []
-    if param[:adigator]
-        funcs = call_adigator(param, graph, I0, param[:verbose])
-    end
-
-    if param[:adigator] && param[:mobility] != 0.5
-        if param[:mobility] == 1 && param[:cong] && !param[:custom]
-            solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_mobility_cgc_ADiGator(x0, auxdata, funcs, verbose)
-        elseif param[:mobility] == 0 && param[:cong] && !param[:custom]
-            solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_cgc_ADiGator(x0, auxdata, funcs, verbose)
-        elseif param[:mobility] == 1 && !param[:cong] && !param[:custom]
-            solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_mobility_ADiGator(x0, auxdata, funcs, verbose)
-        elseif (param[:mobility] == 0 && !param[:cong] && !param[:custom]) && (param[:beta] <= 1 && param[:a] < 1 && param[:duality])
-            solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_by_duality_ADiGator(x0, auxdata, funcs, verbose)
-        elseif (param[:mobility] == 0 && !param[:cong] && !param[:custom]) && (param[:beta] > 1 || param[:a] == 1)
-            solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_ADiGator(x0, auxdata, funcs, verbose)
-        elseif param[:custom]
-            solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_custom_ADiGator(x0, auxdata, funcs, verbose)
+    # else
+    #     if !param[:cong]
+    #         if param[:mobility] == 0
+    #             if param[:beta] <= 1 && param[:duality]
+    #                 solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_by_duality(x0, auxdata, verbose)
+    #             else
+    #                 solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_primal(x0, auxdata, verbose)
+    #             end
+    #         elseif param[:mobility] == 1
+    #             solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_mobility(x0, auxdata, verbose)
+    #         elseif param[:mobility] == 0.5
+    #             solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_partial_mobility(x0, auxdata, verbose)
+    #         end
+    #     else
+    #         if param[:mobility] == 0
+    #             solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_cgc(x0, auxdata, verbose)
+    #         elseif param[:mobility] == 1
+    #             solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_mobility_cgc(x0, auxdata, verbose)
+    #         elseif param[:mobility] == 0.5
+    #             solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_partial_mobility_cgc(x0, auxdata, verbose)
+    #         end
+    #     end
+    # end
+    if haskey(param, :optimizer_attr)
+        for (key, value) in param[:optimizer_attr]
+            set_optimizer_attribute(model, String(key), value)
         end
     else
-        if !param[:cong]
-            if param[:mobility] == 0
-                if param[:beta] <= 1 && param[:duality]
-                    solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_by_duality(x0, auxdata, verbose)
-                else
-                    solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_primal(x0, auxdata, verbose)
-                end
-            elseif param[:mobility] == 1
-                solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_mobility(x0, auxdata, verbose)
-            elseif param[:mobility] == 0.5
-                solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_partial_mobility(x0, auxdata, verbose)
-            end
-        else
-            if param[:mobility] == 0
-                solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_cgc(x0, auxdata, verbose)
-            elseif param[:mobility] == 1
-                solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_mobility_cgc(x0, auxdata, verbose)
-            elseif param[:mobility] == 0.5
-                solve_allocation_handle = (x0, auxdata, funcs, verbose) -> solve_allocation_partial_mobility_cgc(x0, auxdata, verbose)
-            end
-        end
+        # set_optimizer_attribute(model, "tol", 1e-5)  
+        set_optimizer_attribute(model, "max_iter", 10000) 
     end
-
+    # set_attribute(model,
+    #     MOI.AutomaticDifferentiationBackend(),
+    #     MathOptSymbolicAD.DefaultBackend(),
+    # )
+    # if haskey(param, :model_attr) ...
 
     has_converged = false
     counter = 0
@@ -133,39 +133,35 @@ function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbo
 
     while (!has_converged && counter < param[:MAX_ITER_KAPPA]) || counter <= 20
 
-        skip_update = false
+        # auxdata = create_auxdata(param, graph, I0)
 
-        auxdata = create_auxdata(param, graph, I0)
-
-        if save_before_it_crashes
-            debug_file_str = "debug.mat"
-            save(debug_file_str, "param", "graph", "kappa", "x0", "I0", "I1", "counter")
-        end
+        # if save_before_it_crashes
+        #     debug_file_str = "debug.mat"
+        #     save(debug_file_str, "param", "graph", "kappa", "x0", "I0", "I1", "counter")
+        # end
 
         t0 = time()
-        results, flag, x1 = solve_allocation_handle(x0, auxdata, funcs, verbose)
+        optimize!(model)
+        # results, flag, x1 = solve_allocation_handle(x0, auxdata, funcs, verbose)
         t1 = time()
-
-        if !any(flag.status in [0, 1])
-            if !isempty(x0)
-                x0 = []
-                skip_update = true
-            else
-                error("IPOPT returned with error code $(flag.status).")
-            end
-        else
-            x0 = x1
+        if !is_solved_and_feasible(model, allow_almost = true)
+            error("Solver returned with error code $(termination_status(model)).")
         end
 
+        results = recover_allocation(model, auxdata)
+        # Set next run starting values to previous run solution.
+        # vars = all_variables(model)
+        # vars_solution = value.(vars)
+        # set_start_value.(vars, vars_solution)
+        # # Previous: x0 = x1
+        
         if !param[:cong]
-            Pjkn = repeat(permutedims(results[:Pjn], [1, 3, 2]), 1, graph.J, 1)
-            PQ = Pjkn .* results[:Qjkn] .^ (1 + param[:beta])
-            PQ = PQ + permutedims(PQ, [2, 1, 3])
-            PQ = sum(PQ, dims=3)
+            PQ = repeat(results[:Pjn], 1, graph.J, 1) .* results[:Qjkn] .^ (1 + param[:beta])
+            PQ = dropdims(sum(PQ + permutedims(PQ, [2, 1, 3]), dims=3), dims = 3)
             I1 = (graph.delta_tau ./ graph.delta_i .* PQ) .^ (1 / (1 + param[:gamma]))
-            I1[graph.adjacency.==false] .= 0
-            I1[PQ.==0] .= 0
-            I1[graph.delta_i.==0] .= 0
+            I1[graph.adjacency .== 0] .= 0
+            I1[PQ .== 0] .= 0
+            I1[graph.delta_i .== 0] .= 0
         else
             PCj = repeat(results[:PCj], 1, graph.J)
             matm = permutedims(repeat(param[:m], 1, graph.J, graph.J), 2)
@@ -182,8 +178,9 @@ function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbo
         distance_lb = max(maximum(Il - I1), 0)
         distance_ub = max(maximum(I1 - Iu), 0)
         counter_rescale = 0
+
         while distance_lb + distance_ub > TOL_I_BOUNDS && counter_rescale < 100
-            I1 = max(min(I1, Iu), Il)
+            I1 = max.(min.(I1, Iu), Il)
             I1 = param[:K] * I1 / sum(graph.delta_i .* I1)
             distance_lb = max(maximum(Il - I1), 0)
             distance_ub = max(maximum(I1 - Iu), 0)
@@ -194,7 +191,7 @@ function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbo
             println("Warning! Could not impose bounds on network properly.")
         end
 
-        distance = max(abs.(I1 - I0)) / (param[:K] / mean(graph.delta_i[graph.adjacency.==1]))
+        distance = maximum(abs.(I1 - I0)) / (param[:K] / mean(graph.delta_i[graph.adjacency.==1]))
         has_converged = distance < param[:tol_kappa]
         counter += 1
 
@@ -202,7 +199,7 @@ function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbo
             println("Iteration No. $counter distance=$distance duration=$(t1 - t0) secs. Welfare=$(results[:welfare])")
         end
 
-        if (!has_converged || counter <= 20) && !skip_update
+        if !has_converged || counter <= 20
             I0 = weight_old * I0 + (1 - weight_old) * I1
         end
 
@@ -222,7 +219,6 @@ function optimal_network(param, graph, I0=nothing, Il=nothing, Iu=nothing, verbo
     if param[:gamma] > param[:beta] && param[:annealing]
         results = annealing(param, graph, I0, "Funcs" => funcs)
     end
-
 
     return results
 end
