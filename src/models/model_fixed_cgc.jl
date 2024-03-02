a347d2cf2012259e3b77a07a89f140eff45044c8
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-03-01 21:12:47 -0800

    Also adding bounds and dropping dimensions where appropriate.

--- src/models/model_fixed_cgc.jl
+++ src/models/model_fixed_cgc.jl
@@ -17,15 +17,15 @@ function model_fixed_cgc(optimizer, auxdata)
     set_string_names_on_creation(model, false)
 
     # Variables
-    @variable(model, U)                                    # Overall utility
-    @variable(model, Djn[1:graph.J, 1:param.N])            # Consumption per good pre-transport cost (Dj)
-    @variable(model, Qin_direct[1:graph.ndeg, 1:param.N])  # Direct aggregate flow
-    @variable(model, Qin_indirect[1:graph.ndeg, 1:param.N])# Indirect aggregate flow
-    @variable(model, Ljn[1:graph.J, 1:param.N])            # Good specific labour
-    @variable(model, cj[1:graph.J])                        # Overall consumption bundle, including transport costs
+    @variable(model, U)                                             # Overall utility
+    @variable(model, Djn[1:graph.J, 1:param.N] >= 1e-8)             # Consumption per good pre-transport cost (Dj)
+    @variable(model, Qin_direct[1:graph.ndeg, 1:param.N] >= 1e-8)   # Direct aggregate flow
+    @variable(model, Qin_indirect[1:graph.ndeg, 1:param.N] >= 1e-8) # Indirect aggregate flow
+    @variable(model, Ljn[1:graph.J, 1:param.N] >= 1e-8)             # Good specific labour
+    @variable(model, cj[1:graph.J] >= 1e-8)                         # Overall consumption bundle, including transport costs
 
     # Defining Utility Funcion: from cj + parameters (by operator overloading)
-    uj = @expression(model, ((cj / param.alpha) .^ param.alpha .* (param.hj / (1-param.alpha)) .^ (1-param.alpha)) .^ (1-param.rho) / (1-param.rho))
+    @expression(model, uj, ((cj / param.alpha) .^ param.alpha .* (param.hj / (1-param.alpha)) .^ (1-param.alpha)) .^ (1-param.rho) / (1-param.rho))
     U = @expression(model, sum(param.omegaj .* param.Lj .* uj))
     @objective(model, Max, U)
 
@@ -38,16 +38,16 @@ function model_fixed_cgc(optimizer, auxdata)
                     cj[j] * param.Lj[j] + 
                     max(A[j, i], 0) * B_direct + 
                     max(-A[j, i], 0) * B_indirect - 
-                    sum(Djn[j, n] ^ psigma for n in 1:param.N) ^ (1 / psigma) <= 1e-8
+                    sum(Djn[j, n] ^ psigma for n in 1:param.N) ^ (1 / psigma) <= -1e-8
         )
     end
 
     # Balanced flow constraints
-    Yjn = @expression(model, param.Zjn .* (Ljn .^ param.a))
+    @expression(model, Yjn, param.Zjn .* (Ljn .^ param.a))
     @constraint(model, Pjn, Djn + A * Qin_direct - A * Qin_indirect - Yjn .<= -1e-8)
 
     # Local labor availability constraints ( sum Ljn <= Lj )
-    @constraint(model, -1e-8 .<= sum(Ljn, dims=2) .- Lj .<= 1e-8)
+    @constraint(model, sum(Ljn, dims=2) .- Lj .<= -1e-8)
 
     return model
 end
@@ -60,18 +60,18 @@ function recover_allocation_fixed_cgc(model, auxdata)
 
     results[:welfare] = value(model_dict[:U])
     results[:Yjn] = value.(model_dict[:Yjn])
-    results[:Yj] = sum(results[:Yjn], dims=2) 
+    results[:Yj] = dropdims(sum(results[:Yjn], dims=2), dims = 2) 
     results[:Ljn] = value.(model_dict[:Ljn])
     results[:Lj] = param.Lj
     results[:Djn] = value.(model_dict[:Djn]) # Consumption per good pre-transport cost
-    results[:Dj] = sum(results[:Djn] .^ ((param.sigma-1)/param.sigma), dims=2) .^ (param.sigma/(param.sigma-1))
+    results[:Dj] = dropdims(sum(results[:Djn] .^ ((param.sigma-1)/param.sigma), dims=2), dims = 2) .^ (param.sigma/(param.sigma-1))
     results[:cj] = value.(model_dict[:cj])
     results[:Cj] = results[:cj] .* param.Lj
     results[:hj] = ifelse.(results[:Lj] .== 0, 0.0, param.Hj ./ results[:Lj])
     results[:uj] = value.(model_dict[:uj])
     # Prices
     results[:Pjn] = shadow_price.(model_dict[:Pjn])
-    results[:PCj] = sum(results[:Pjn] .^ (1-param.sigma), dims=2) .^ (1/(1-param.sigma))    
+    results[:PCj] = dropdims(sum(results[:Pjn] .^ (1-param.sigma), dims=2), dims = 2) .^ (1/(1-param.sigma))    
     # Network flows
     Qin_direct = value.(model_dict[:Qin_direct])
     Qin_indirect = value.(model_dict[:Qin_indirect])
