00603fb9dafa0d2fdfca84fc82c52851e0dd1a35
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-03-01 22:30:33 -0800

    Edit example 1: ensure compatibility with Julia library.

--- examples/example01.jl
+++ examples/example01.jl
@@ -5,14 +5,13 @@ using OptimalTransportNetworks
 # INITIALIZATION
 # ==============
 
-# Set parameters: try with LaborMobility: on/off, convex: beta>=gamma,
-# nonconvex: gamma>beta, CrossGoodCongestion: on/off, or ADiGator: on/off
+# Set parameters: try with labor mobility: true/false, convex: beta>=gamma,
+# nonconvex: gamma>beta, cross good congestion: true/false, or ADiGator: on/off
 
-param = init_parameters(LaborMobility="on", K=10, gamma=1, beta=1,
-                        N=1, TolKappa=1e-5, ADiGator=true, CrossGoodCongestion=false, nu=1)
+param = init_parameters(labor_mobility = true, K = 10, gamma = 1, beta = 1, verbose = true,
+                        N = 1, kappa_tol = 1e-5, cross_good_congestion = true, nu = 1)
 
-
-# TolKappa is the tolerance in distance b/w iterations for road capacity
+# kappa_tol is the tolerance in distance b/w iterations for road capacity
 # kappa=I^gamma/delta_i, default is 1e-7 but we relax it a bit here
 
 # ------------
@@ -30,17 +29,11 @@ param[:Zjn][Ni] = 1 # central node more productive
 # RESOLUTION
 # ==========
 
-@time results = optimal_network(param, graph)
-
-# Plot them 
-
-sizes = 1.5 * (results["Cj"] .- minimum(results["Cj"])) ./ (maximum(results["Cj"]) - minimum(results["Cj"])) # size of each node
-shades = (results["Cj"] .- minimum(results["Cj"])) ./ (maximum(results["Cj"]) - minimum(results["Cj"])) # shading for each node in [0,1] between NodeBgColor and NodeFgColor
-
-plot_graph(param, graph, results["Ijk"], "Sizes" => sizes, "Shades" => shades,
-    "NodeFgColor" => [1 .9 .4], "NodeBgColor" => [.8 .1 .0], "NodeOuterColor" => [0 .5 .6])
-
-# the size/shade of the nodes reflects the total consumption at each node
+@time results = optimal_network(param, graph) # solve the network
 
 
-# Please note that this translation assumes that the `OptimalTransportNetworks` package in Julia has similar functions and data structures as the MATLAB code. If this is not the case, the translation may need to be adjusted accordingly.
+# Plot the network with the optimal transportation plan
+
+plot_graph(graph, results[:Ijk], node_sizes = results[:Cj])
+
+# The size/shade of the nodes reflects the total consumption at each node
