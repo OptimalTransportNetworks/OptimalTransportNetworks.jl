b36332c34703fce8bd131fcbcfc0f97564f220f5
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-03-05 16:57:37 -0800

    Update remove_node().

--- src/main/nodes.jl
+++ src/main/nodes.jl
@@ -108,51 +108,51 @@ Removes node i from the graph.
 Returns the updated graph and param structure (param is affected too because the variable Zjn, Lj, Hj and others are changed).
 """
 function remove_node(param, graph, i)
+    
     if i < 1 || i > graph.J || i != floor(i)
-        println("remove_node: node i should be an integer between 1 and $(graph.J).")
-        return
+        error("remove_node: node i should be an integer between 1 and $(graph.J).")
     end
 
-    # create new nodes structure
-    nodes = [Dict() for _ in 1:graph.J-1]
-    x = zeros(graph.J-1)
-    y = zeros(graph.J-1)
+    Jnew = graph.J - 1
+    graph_new = Dict(pairs(graph))
 
-    for k in 1:graph.J-1
-        cursor = k + (k >= i)
+    graph_new[:J] = Jnew
+    graph_new[:x] = deleteat!(copy(graph.x), i)
+    graph_new[:y] = deleteat!(copy(graph.y), i)
+    graph_new[:nodes] = deleteat!(copy(graph.nodes), i)
 
-        nodes[k]["neighbors"] = setdiff(graph.nodes[cursor]["neighbors"], i) # remove this node from others' neighbors.
-        nodes[k]["neighbors"] = [n-1 for n in nodes[k]["neighbors"] if n > i] # reindex nodes k > i to k-1
-
-        x[k] = graph.x[cursor]
-        y[k] = graph.y[cursor]
+    nodes = graph_new[:nodes]
+    for k in 1:Jnew
+        node_k = filter(x -> x != i, nodes[k]) # nodes[k][nodes[k] .!= i]
+        nodes[k] = ifelse.(node_k .> i, node_k .- 1, node_k) # reindex nodes k > i to k-1
     end
 
     # rebuild adjacency matrix, delta_i and delta_tau
-    adjacency = [graph.adjacency[1:i-1, 1:i-1] graph.adjacency[1:i-1, i+1:end];
-                 graph.adjacency[i+1:end, 1:i-1] graph.adjacency[i+1:end, i+1:end]]
+    graph_new[:adjacency] = [graph.adjacency[1:i-1, 1:i-1] graph.adjacency[1:i-1, i+1:end];
+                             graph.adjacency[i+1:end, 1:i-1] graph.adjacency[i+1:end, i+1:end]]
 
-    delta_i = [graph.delta_i[1:i-1, 1:i-1] graph.delta_i[1:i-1, i+1:end];
-               graph.delta_i[i+1:end, 1:i-1] graph.delta_i[i+1:end, i+1:end]]
+    graph_new[:delta_i] = [graph.delta_i[1:i-1, 1:i-1] graph.delta_i[1:i-1, i+1:end];
+                           graph.delta_i[i+1:end, 1:i-1] graph.delta_i[i+1:end, i+1:end]]
 
-    delta_tau = [graph.delta_i[1:i-1, 1:i-1] graph.delta_i[1:i-1, i+1:end];
-                 graph.delta_i[i+1:end, 1:i-1] graph.delta_i[i+1:end, i+1:end]]
+    graph_new[:delta_tau] = [graph.delta_tau[1:i-1, 1:i-1] graph.delta_tau[1:i-1, i+1:end];
+                             graph.delta_tau[i+1:end, 1:i-1] graph.delta_tau[i+1:end, i+1:end]]
 
-    ndeg = sum(tril(adjacency))
+    graph_new[:ndeg] = sum(tril(graph_new[:adjacency]))
 
-    # return new graph structure
-    graph = Dict("J" => graph.J-1, "x" => x, "y" => y, "nodes" => nodes, "adjacency" => adjacency,
-                 "delta_i" => delta_i, "delta_tau" => delta_tau, "ndeg" => ndeg)
+    if graph.region !== nothing
+        graph_new[:region] = deleteat!(copy(graph.region), i)
+    end
 
-    # now, update the param structure
-    param["J"] = graph["J"]
-    param["Lj"] = vcat(param["Lj"][1:i-1], param["Lj"][i+1:end])
-    param["Hj"] = vcat(param["Hj"][1:i-1], param["Hj"][i+1:end])
-    param["hj"] = vcat(param["hj"][1:i-1], param["hj"][i+1:end])
-    param["omegaj"] = vcat(param["omegaj"][1:i-1], param["omegaj"][i+1:end])
-    param["Zjn"] = vcat(param["Zjn"][1:i-1, :], param["Zjn"][i+1:end, :])
+    # Now, update the param structure
+    param_new = copy(param)
+    param_new[:J] = Jnew
+    param_new[:Lj] = vcat(param[:Lj][1:i-1], param[:Lj][i+1:end])
+    param_new[:Hj] = vcat(param[:Hj][1:i-1], param[:Hj][i+1:end])
+    param_new[:hj] = vcat(param[:hj][1:i-1], param[:hj][i+1:end])
+    param_new[:omegaj] = vcat(param[:omegaj][1:i-1], param[:omegaj][i+1:end])
+    param_new[:Zjn] = vcat(param[:Zjn][1:i-1, :], param[:Zjn][i+1:end, :])
 
-    return param, graph
+    return param_new, graph_new
 end
 
 
