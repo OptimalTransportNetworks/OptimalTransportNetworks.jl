830a5b187027693154f986e6d1c014f6b7a7f611
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2024-03-14 23:40:27 -0700

    Setting defaults.

--- src/main/create_graph.jl
+++ src/main/create_graph.jl
@@ -29,7 +29,7 @@ Initialize the underlying graph, population and productivity parameters.
 graph = create_graph(init_parameters())
 ```
 """
-function create_graph(param, w, h; type = "map", kwargs...)
+function create_graph(param, w = 10, h = 10; type = "map", kwargs...)
 
     options = retrieve_options_create_graph(param, w, h, type; kwargs...)
 
